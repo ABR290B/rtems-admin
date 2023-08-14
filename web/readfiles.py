f0d3677bf00c41aa5cd72d4962af0a4695d010c0
Abhimanyu Raghuvanshi <araghuvanshi_be20@thapar.edu>
2023-08-14 23:04:55 +0530

    Added Tests Report

--- web/readfiles.py
+++ web/readfiles.py
@@ -93,6 +93,216 @@ def tool_build_parse(tool_build_results):
 
     return tool_build_details
 
+
+def test_parse(test_results, file_path):
+    test_details = []
+
+    for entry in test_results:
+        message_id = entry.get('Message-ID', '')
+        #print("Message-ID:", message_id)  # Print MessageID for debugging
+        #print ("File Path:", file_path)  #File path for debugging
+        test_data = test_report(message_id, file_path)
+        if test_data:
+            test_details.append(test_data)
+    
+    return test_details
+
+def test_report(message_id, file_path):
+    try:
+        with open(file_path, 'r') as archive_file:
+            archive_data = archive_file.read()
+
+            # Locate the start index of the message ID value
+            message_id_idx = archive_data.find(message_id)
+            if message_id_idx == -1:
+                print(f"Message-ID '{message_id}' not found in the archive.")
+                return None
+            
+            # Locate the start index of the message section
+            start_idx = archive_data.rfind("Message-ID:", 0, message_id_idx)
+
+            # Locate the end index of the message section
+            end_idx = archive_data.find("From:", start_idx)
+            if end_idx == -1:
+                end_idx = len(archive_data)
+
+            # Extract the content of the located section
+            message_content = archive_data[start_idx:end_idx].strip()
+
+            # Split the content into lines
+            lines = message_content.split('\n')
+            #print("Message content lines:")
+            #for line in lines:
+                #print(line)
+            # Find the index of the "Summary" and "Failures" sections
+            summary_start_idx = lines.index("Summary")
+            failures_start_idx = lines.index("Failures:")
+
+            # Extract the "Summary" section
+            summary_lines = lines[summary_start_idx + 2:failures_start_idx - 1]
+            #print(summary_lines)
+            # Find the index of the next section (e.g., "Log")
+            next_section_start_idx = lines.index("Log")
+
+            # Extract the "Failures" section
+            failures_lines = lines[failures_start_idx + 1:next_section_start_idx - 1]
+            
+            # Initialize variables to store parsed data
+            host = None
+            # Initialize variables for each category
+            passed_count = 0
+            failed_count = 0
+            user_input_count = 0
+            expected_fail_count = 0
+            indeterminate_count = 0
+            benchmark_count = 0
+            timeout_count = 0
+            invalid_count = 0
+            wrong_version_count = 0
+            wrong_build_count = 0
+            wrong_tools_count = 0
+
+            # Initialize lists for failure categories
+            failed_failures = []
+            user_input_failures = []
+            expected_fail_failures = []
+            indeterminate_failures = []
+            benchmark_failures = []
+            timeout_failures = []
+            invalid_failures = []
+            wrong_version_failures = []
+            wrong_build_failures = []
+            wrong_tools_failures = []
+
+            for line in lines:
+                if line.startswith('Host:'):
+                        #print(line)
+                        host_line = line.split(':', 1)
+                        host = host_line[1].split(None, 1)[0]
+            # Iterate through the lines and parse the data
+            for line in summary_lines:
+                #print(f"Processing line: {line}")  # Print the line for debugging
+                    if line.startswith('Passed:'):
+                        passed_count = int(line.split(':', 1)[1].strip())
+                    elif line.startswith('Failed:'):
+                        failed_count = int(line.split(':', 1)[1].strip())
+                    elif line.startswith('User Input:'):
+                        #print(line)
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            user_input_count = int(match.group(1))
+                        else:
+                            print("Error parsing User Input count")
+                    elif line.startswith('Expected Fail:'):
+                        expected_fail_count = int(line.split(':', 1)[1].strip())
+                    elif line.startswith('Indeterminate:'):
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            indeterminate_count = int(match.group(1))
+                        else:
+                            print("Error parsing Indeterminate count")
+                    elif line.startswith('Benchmark:'):
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            benchmark_count = int(match.group(1))
+                        else:
+                            print("Error parsing Benchmark count")
+                    elif line.startswith('Timeout:'):
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            timeout_count = int(match.group(1))
+                        else:
+                            print("Error parsing Timeout count")
+                    elif line.startswith('Invalid:'):
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            invalid_count = int(match.group(1))
+                        else:
+                            print("Error parsing Invalid count")
+                    elif line.startswith('Wrong Version:'):
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            wrong_version_count = int(match.group(1))
+                        else:
+                            print("Error parsing Wrong Version count")
+                    elif line.startswith('Wrong Build:'):
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            wrong_build_count = int(match.group(1))
+                        else:
+                            print("Error parsing Wrong Build count")
+                    elif line.startswith('Wrong Tools:'):
+                        match = re.search(r'(\d+)', line)
+                        if match:
+                            wrong_tools_count = int(match.group(1))
+                        else:
+                            print("Error parsing Wrong Tools count")
+                                # Similarly, handle other failure categories
+            
+            # Initialize the current category
+            current_category = "Failed"
+
+            # Loop through the failure lines and categorize the failures
+            for line in failures_lines:
+                if line.endswith(":"):
+                    current_category = line.strip(":")  # Extract the category name
+                elif current_category:
+                    # Append the failure to the corresponding category list
+                    if current_category == "Failed":
+                        failed_failures.append(line)
+                    elif current_category == "User Input":
+                        user_input_failures.append(line)
+                    elif current_category == "Expected Fail":
+                        expected_fail_failures.append(line)
+                    elif current_category == "Indeterminate":
+                        indeterminate_failures.append(line)
+                    elif current_category == "Benchmark":
+                        benchmark_failures.append(line)
+                    elif current_category == "Timeout":
+                        timeout_failures.append(line)
+                    elif current_category == "Invalid":
+                        invalid_failures.append(line)
+                    elif current_category == "Wrong Version":
+                        wrong_version_failures.append(line)
+                    elif current_category == "Wrong Build":
+                        wrong_build_failures.append(line)
+                    elif current_category == "Wrong Tools":
+                        wrong_tools_failures.append(line)
+              
+
+            # Create the dictionary with parsed data
+            test_dict = {"Host":host, 
+                         "Passed Count":passed_count, 
+                         "Failed Count":failed_count, 
+                         "User Input Count": user_input_count,
+                         "Expectd Fail Count": expected_fail_count,
+                         "Indeterminate Count": indeterminate_count,
+                         "Benchmark Count": benchmark_count,
+                         "Timeout Count": timeout_count,
+                         "Invalid Count": invalid_count,
+                         "Wrong Version Count": wrong_version_count,
+                         "Wrong Build Count": wrong_build_count,
+                         "Wrong Tools Count": wrong_tools_count,
+                         "Failed Tests": failed_failures,
+                         "User Input ": user_input_failures,
+                         "Expected Fails": expected_fail_failures,
+                         "Indeterminate": indeterminate_failures,
+                         "Benchmark": benchmark_failures,
+                         "Timeout": timeout_failures,
+                         "Invalid": invalid_failures,
+                         "Wrong Version": wrong_version_failures,
+                         "Wrong Build": wrong_build_failures,
+                         "Wrong Tools": wrong_tools_failures
+                         }
+            
+            
+            return test_dict
+
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+
+
 def visualise_tool_results(tool_build_details):
     # Sort the tool_build_details by date
     tool_build_details = sorted(tool_build_details, key=lambda entry: entry['Date'])
@@ -162,15 +372,6 @@ def Monthly_Build_Summary(tool_build_details):
     return month_build_summary
 
 
-def bsp_build(archives):
-    bspreports = {}
-    return bspreports
-
-def test_builds(archives):
-    testreports = {}
-    return testreports
-
-
 def main():
     try:
         year = int(input("Please enter the year for which you wish to see the reports: "))
@@ -190,11 +391,16 @@ def main():
         data = read_data_from_file(path)
         
         # Categorize the data
-        tool_build_results, _, _ = categorize_data(data)
+        tool_build_results, test_results, bsp_results = categorize_data(data)
 
         # Parse tool build results
         tool_build_details = tool_build_parse(tool_build_results)
-
+        # Parse test results
+        test_details = test_parse(test_results,path)
+        output_file_path = 'web/json-files/tests/test_report.json'
+        with open(output_file_path, 'w') as output_file:
+            json.dump(test_details, output_file, indent=4)
+        print("Test Details saved to:", output_file_path)
         # Save parsed data to JSON file
         output_file_path = 'web/json-files/tools/tool_build_report.json'
         with open(output_file_path, 'w') as output_file:
